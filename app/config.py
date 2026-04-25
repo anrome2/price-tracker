7fc28dd8da39cca309a56181471887c5d720397c
andreaTWD <c.datos14@salascala.com>
2026-04-25 16:37:19 +0200

    El Google Sheet lo crea aunque se haya borrado y esté en Papelera

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
 ### BASES DE DATOS
 DB_NAME = 'productos.db'
 TABLE_DB_NAME = 'productos'
+
+### GOOGLE SHEETS
+ID_CARPETA = "1HFSvwifxvicibM5MLmvzL3KujE-MlKK7"
+EMAIL = "andrearomeromedina@gmail.com" # Cambia esto por tu cuenta real
+NOMBRE_SHEET = "Price-Tracker"
--- app/sheets.py
+++ app/sheets.py
@@ -2,11 +2,12 @@ import gspread
 from gspread.exceptions import SpreadsheetNotFound, APIError
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
-ID_CARPETA = "1SoGjTGcs56L_xJPdZjSfkBgRxJMB4Jjn"
+from app.config import EMAIL, ID_CARPETA, NOMBRE_SHEET
 
-# 1. CONFIGURACIÓN DE CREDENCIALES
-def conectar_google():
+# CREDENCIALES
+def credenciales():
     # El 'scope' define qué permisos estamos pidiendo
     scope = [
         "https://www.googleapis.com/auth/spreadsheets",
@@ -17,50 +18,68 @@ def conectar_google():
     flow = InstalledAppFlow.from_client_secrets_file(
         '.secrets/oauth_credentials.json', scope)
     creds = flow.run_local_server(port=0)
+    return creds
 
+
+# CONFIGURACIÓN DE CREDENCIALES
+def conectar_google(creds):
     client = gspread.authorize(creds)
     return client
 
-# 2. LÓGICA DE CREACIÓN O APERTURA
-def obtener_o_crear_hoja(nombre_archivo, mi_email):
-    gc = conectar_google()
-    
-    try:
-        # 1. Intentamos abrir el archivo por nombre
-        # Nota: Si hay varios con el mismo nombre, abrirá el más reciente
-        spreadsheet = gc.open(nombre_archivo)
-        print(f"✅ Archivo '{nombre_archivo}' encontrado y abierto.")
-        
-    except SpreadsheetNotFound:
-        # 2. Si no existe, procedemos a crearlo dentro de la carpeta específica
-        print(f"⚠️ El archivo no existe. Creando '{nombre_archivo}' en la carpeta autorizada...")
-        
-        try:
-            # Creamos el archivo indicando el folder_id para usar TU cuota de espacio
-            spreadsheet = gc.create(nombre_archivo, folder_id=ID_CARPETA)
+# LÓGICA DE CREACIÓN O APERTURA
+def obtener_o_crear_hoja():
+    creds = credenciales()
+    gc = conectar_google(creds=creds)
+      
+    # 1. Intentamos abrir el archivo por nombre y el ID de la carpeta
+    # Nota: Si hay varios con el mismo nombre, abrirá el más reciente         
+    files = buscar_archivo_en_carpeta(creds=creds)
+
+    spreadsheet = None
+    for f in files:
+        spreadsheet = gc.open_by_key(f['id'])
+        print(f"✅ Archivo '{NOMBRE_SHEET}' encontrado y abierto.")
+        break    
+       
+    # 2. Si no existe, procedemos a crearlo dentro de la carpeta específica
+    if spreadsheet is None:
+        print(f"⚠️ El archivo no existe. Creando '{NOMBRE_SHEET}' en la carpeta autorizada...")                  
+        try:             
+            # Creamos el archivo indicando el folder_id para usar TU cuota de espacio             
+            spreadsheet = gc.create(NOMBRE_SHEET, folder_id=ID_CARPETA)                          
+            # 3. Lo compartimos con tu email para que seas el dueño/editor visible             
+            # # Aunque esté en tu carpeta, el "creador" técnico es el bot             
+            spreadsheet.share(EMAIL, perm_type='user', role='writer')                          
+            print(f"🚀 Archivo creado con éxito y compartido con {EMAIL}")
             
-            # 3. Lo compartimos con tu email para que seas el dueño/editor visible
-            # Aunque esté en tu carpeta, el "creador" técnico es el bot
-            spreadsheet.share(mi_email, perm_type='user', role='writer')
-            
-            print(f"🚀 Archivo creado con éxito y compartido con {mi_email}")
-            
+            spreadsheet.sheet1.update(values=[['NOMBRE', 'URL']], range_name='A1:B1')
+                
         except APIError as e:
             print(f"❌ Error crítico de la API de Google: {e}")
-            return None
-        except Exception as e:
-            print(f"❌ Ocurrió un error inesperado: {e}")
             return None
 
     # Retornamos la primera hoja (tab) del archivo
     return spreadsheet.sheet1
 
-# 3. EJECUCIÓN
-MI_EMAIL_PERSONAL = "andrxme2@gmail.com" # Cambia esto por tu cuenta real
-nombre_del_sheet = "Price-Tracker"
+def buscar_archivo_en_carpeta(creds):
+    
+    service = build('drive', 'v3', credentials=creds)
 
-hoja = obtener_o_crear_hoja(nombre_del_sheet, MI_EMAIL_PERSONAL)
+    query = f"""
+        name = '{NOMBRE_SHEET}'
+        and mimeType = 'application/vnd.google-apps.spreadsheet'
+        and '{ID_CARPETA}' in parents
+        and trashed = false
+    """
 
-# Prueba: Escribir algo en la primera fila
-# hoja.update('A1', [['ID', 'Nombre', 'Fecha']])
-# print("¡Proceso completado!")
+    results = service.files().list(
+        q=query,
+        spaces='drive',
+        fields='files(id, name)'
+    ).execute()
+
+    return results.get('files', [])
+
+# EJECUCIÓN DE PRUEBA
+if __name__ == "__main__":
+    hoja = obtener_o_crear_hoja()
